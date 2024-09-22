dfb0f3b34fdae6c6321ce2aa9a5ce58f977b5c6a
Johappy07 <johanesberchmann@gmail.com>
2024-09-22 23:43:35 +0700

    update acara 5

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import Map from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
+import Graphic from '@arcgis/core/Graphic';
+import Point from '@arcgis/core/geometry/Point';
+import ImageryLayer from '@arcgis/core/layers/ImageryLayer';
+import SimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
 import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
@@ -9,29 +13,59 @@ import { Geolocation } from '@capacitor/geolocation';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-
-  latitude!: number;
-  longitude!: number;
+  mapView: MapView | any;
+  userLocationGraphic: Graphic | any;
 
   constructor() {}
 
-  async ngOnInit(): Promise<void> {
-    // Get the current position
-    const position = await Geolocation.getCurrentPosition();
-    this.latitude = position.coords.latitude;
-    this.longitude = position.coords.longitude;
-
+  async ngOnInit() {
     // Initialize the map
     const map = new Map({
-      basemap: 'topo-vector'
+      basemap: 'topo-vector' // Add functionality to change basemap later
     });
 
-    // Initialize the map view with updated latitude and longitude
-    const view = new MapView({
+    // Create the view
+    this.mapView = new MapView({
       container: 'container',
       map: map,
-      zoom: 4,
-      center: [this.longitude, this.latitude] // Corrected order: [longitude, latitude]
+      zoom: 12
     });
+
+    // Add the weather layer (Radar Base Reflectivity Time)
+    const weatherLayer = new ImageryLayer({
+      url: 'https://mapservices.weather.noaa.gov/eventdriven/rest/services/radar/radar_base_reflectivity_time/ImageServer'
+    });
+    map.add(weatherLayer);
+
+    // Update user location and display marker
+    await this.updateUserLocationOnMap();
+    this.mapView.center = this.userLocationGraphic.geometry as Point;
+    setInterval(this.updateUserLocationOnMap.bind(this), 10000);
+  }
+
+  async getLocationService(): Promise<number[]> {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition((resp) => {
+        resolve([resp.coords.latitude, resp.coords.longitude]);
+      });
+    });
+  }
+
+  async updateUserLocationOnMap() {
+    let latLng = await this.getLocationService();
+    let geom = new Point({ latitude: latLng[0], longitude: latLng[1] });
+    if (this.userLocationGraphic) {
+      this.userLocationGraphic.geometry = geom;
+    } else {
+      // Create marker at user location
+      this.userLocationGraphic = new Graphic({
+        symbol: new SimpleMarkerSymbol({
+          color: [226, 119, 40],
+          outline: { color: [255, 255, 255], width: 1 }
+        }),
+        geometry: geom
+      });
+      this.mapView.graphics.add(this.userLocationGraphic);
+    }
   }
 }
